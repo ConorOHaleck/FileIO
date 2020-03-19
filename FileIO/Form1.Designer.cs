e00a43a337993f72af5a536da02143e89b3b86fe
ofHillsAndLakes <conorohaleck@gmail.com>
2020-03-19 00:49:13 -0700

    Add project files.

---
+++ FileIO/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FileIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(12, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(155, 43);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import File";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Enabled = false;
+            this.btnEncrypt.Location = new System.Drawing.Point(173, 14);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(155, 43);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFile.Enabled = false;
+            this.txtFile.Location = new System.Drawing.Point(85, 130);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(243, 26);
+            this.txtFile.TabIndex = 2;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Enabled = false;
+            this.btnDecrypt.Location = new System.Drawing.Point(173, 63);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(155, 43);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(12, 63);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(155, 43);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save File";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 178);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnImport);
+            this.Name = "Form1";
+            this.Text = "File I/O Project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FileIO/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] fileLines;
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog OFDialog = new OpenFileDialog())
+            {
+                OFDialog.InitialDirectory = "c:\\";
+                OFDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                OFDialog.FilterIndex = 2;
+                OFDialog.RestoreDirectory = true;
+
+                if(OFDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string pathway = OFDialog.FileName;
+                    fileLines = File.ReadAllLines(pathway);
+
+                    btnEncrypt.Enabled = true;
+                    btnSave.Enabled = true;
+                    txtFile.Text = pathway;
+                    txtFile.BackColor = Color.PaleGreen;
+
+                    MessageBox.Show("File successfully read!", "Good job", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+            {
+
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 2;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllLines(saveFileDialog1.FileName, fileLines);
+                }
+            }
+        }
+
+        public string EncryptString(string toEncrypt)
+        {
+            byte[] b = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncrypt);
+            string encrypted = Convert.ToBase64String(b);
+            Console.WriteLine(encrypted);
+            return encrypted;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < fileLines.Length; i++)
+            {
+                fileLines[i] = EncryptString(fileLines[i]);
+            }
+
+            txtFile.BackColor = Color.IndianRed;
+            btnEncrypt.Enabled = false;
+            btnDecrypt.Enabled = true;
+        }
+
+        public string DecryptString(string toDecrypt)
+        {
+            byte[] b;
+            string returner;
+            try
+            {
+                b = Convert.FromBase64String(toDecrypt);
+                returner = System.Text.ASCIIEncoding.ASCII.GetString(b);
+            }
+            catch(FormatException fe)
+            {
+                returner = "";
+            }
+            Console.WriteLine(returner);
+            return returner;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < fileLines.Length; i++)
+            {
+                fileLines[i] = DecryptString(fileLines[i]);
+            }
+
+            txtFile.BackColor = Color.PaleGreen;
+            btnEncrypt.Enabled = true;
+            btnDecrypt.Enabled = false;
+        }
+    }
+}
